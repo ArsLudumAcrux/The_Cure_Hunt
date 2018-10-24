41c90b4a88e46de5be6a1cb45a09c19b1a69c029
ArsLudumAcrux <43455385+ArsLudumAcrux@users.noreply.github.com>
2018-10-24 00:18:37 -0300

    Barra de Xp 24-10
    tentativa falha de arumar a barra de Xp

--- The Cure hunt (0.5.7)/Assets/Scripts/Player/ExpBar.cs
+++ The Cure hunt (0.5.7)/Assets/Scripts/Player/ExpBar.cs
@@ -5,37 +5,43 @@ using UnityEngine.UI;
 
 public class ExpBar : MonoBehaviour {
 
-    public Statistics stat;
-    public Image experiencia_img;
+    public Statistics stat;//variavel referente ao script Statistics
+    Image experiencia_img;
     public float expCur;
     //Slime_Stats slime_stats;
 
     public void Start()
     {
-        stat = FindObjectOfType<Statistics>();
-        experiencia_img = GetComponent<Image>();
+        stat = FindObjectOfType<Statistics>();//pegando o script Statistics
+        experiencia_img = GetComponent<Image>();// Pegando a imagem pra utilizar o fillAmount   
+        //experiencia_img.fillAmount = 0;
     }
     public void Update()
     {
-        expCur = stat.ExpAtual / stat.XPToNextLevel;
-        experiencia_img.fillAmount = expCur;
+        experiencia_img.fillAmount = stat.XPToNextLevel / stat.ExpAtual ;
 
-        //if (stat.ExpAtual == stat.XPToNextLevel) { 
-        //    stat.ExpAtual = 0;
+        //if (experiencia_img.fillAmount == 1)
+        //{
+        //    experiencia_img.fillAmount = 0;
         //}
 
-        if (stat.ExpAtual >= stat.XPToNextLevel)
-        {
-            stat.Level = stat.Level + 1;
-            stat.XPToNextLevel = Mathf.Round(stat.XPToNextLevel + (stat.XPToNextLevel * 1.63f));
-            stat.HP_Max = Mathf.Round(stat.HP_Max * 1.05f);
+        if (stat.ExpAtual >= stat.XPToNextLevel)//caso a experiencia atual seja igual xp necessario para o proximo nivel execulta as ações abaixo
+        {        
+            stat.Level = stat.Level + 1;//level atual +1
+            stat.XPToNextLevel = Mathf.Round(stat.XPToNextLevel + (stat.XPToNextLevel * 1.63f));//xp necessario para o proximo nivel aumenta
+            stat.HP_Max = Mathf.Round(stat.HP_Max * 1.05f);//Vida maxima aumenta 5% a cada nivel do player
             //source.PlayOneShot(LevelUp, 1.0f);
-            stat.LevelText.text = stat.Level.ToString();
+            stat.LevelText.text = stat.Level.ToString();//text do cavas atualiza a cada nivel e mostra o nivel atual
             //stat.ExpAtual = 0;
             experiencia_img.fillAmount = 0f;
+            //Printa o level e vida maxima do player
             PlayerPrefs.SetInt("LevelDoPlayer", stat.Level);
-            Debug.Log("LevelDoPlayer" + stat.Level);
+            //Debug.Log("LevelDoPlayer" + stat.Level);
             Debug.Log("Vida maxima atual: " + stat.HP_Max);
+            if (experiencia_img.fillAmount == 1)
+            {
+                experiencia_img.fillAmount = 0;
+            }
         }
     }
     public void Experiencia(int xpMin, int xpMax)
