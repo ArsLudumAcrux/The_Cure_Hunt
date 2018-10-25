9de91f6c26681222361c2e887d51523b054cfbce
ArsLudumAcrux <backupthecurehunt@gmail.com>
2018-10-25 15:08:54 -0300

    Gemas
    Gemas

--- The Cure hunt (0.5.7)/Assets/Scripts/Magic.cs
+++ The Cure hunt (0.5.7)/Assets/Scripts/Magic.cs
@@ -1,19 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Magic : MonoBehaviour {
 
-	// Use this for initialization
-	public void MagiaAtual(string name)
+    [Header ("Gemas")]
+    public Button GemaFloresta;
+    public Button GemaFogo;
+    [Header("Gema Atual")]
+    public Image GemaAtual;
+    public Sprite GemaVerde;
+    public Sprite GemaVerm;
+    [Header("Gemas Bloqueadas")]
+    public Button[] GemasBloqueadas;
+    public Text[] Text;
+
+    // Use this for initialization
+
+
+
+
+
+    public void Start()
+    {
+        GemaFloresta.gameObject.SetActive(false);
+        GemaFogo.gameObject.SetActive(false);
+        GemasBloqueadas[0].gameObject.SetActive(true);
+        GemasBloqueadas[1].gameObject.SetActive(true);
+    }
+    public void Update()
+    {
+
+    }
+    public void GemasBloq(string name)
+    {
+        if(name == "Bloq1")
+        {
+        }
+        if (name == "Bloq2")
+        {
+        }
+        if (name == "Bloq3")
+        {
+        }
+        if (name == "Bloq4")
+        {
+        }
+        if (name == "Bloq5")
+        {
+        }
+        if (name == "Bloq6")
+        {
+        }
+    }
+
+    public void MagiaAtual(string name)
     {
         if(name == "Floresta")
         {
-
+            GemaAtual.GetComponent<Image>().sprite = GemaVerde;
+            print("GEMA VERDE " + name);
         }
         if (name == "Fogo")
         {
-
+            GemaAtual.GetComponent<Image>().sprite = GemaVerm;
         }
     }
 
